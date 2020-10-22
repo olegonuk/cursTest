35722f09ec605cf448586a35fd6d56c41e8e7ecf
Oleh Denysov <olegonuk@gmail.com>
2020-10-22 17:24:13 +0300

    29_Lessons

---
+++ 29_Lesson/js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const box = document.getElementById('box'),
+   btns = document.getElementsByTagName('button'),
+   circles = document.getElementsByClassName('circle'),
+   hearts = document.querySelectorAll('.heart'),
+   oneHeart = document.querySelector('.heart'),
+   wrapper = document.querySelector('.wrapper');
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = `background-color: blue; width: 500px`;
+
+btns[1].style.borderRadius = '100%';
+circles[0].style.backgroundColor = 'red';
+
+hearts.forEach(item => {
+   item.style.backgroundColor = 'blue';
+})
+
+const div = document.createElement('div');
+// const text = document.createTextNode('Тут былл я');
+div.classList.add('black');
+
+wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+// wrapper.insertBefore(div, heart[1]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]); 
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello world</h1>";
+// div.textContent = 'Hello';
+div.insertAdjacentHTML('afterbegin', '<h2>Hello</h2>');
+
+
+
