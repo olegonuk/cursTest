e1a53e918c830911f233ae13de7db9bf9212de5b
Oleh Denysov <olegonuk@gmail.com>
2020-11-04 10:58:47 +0200

    52 Lesson

---
+++ 52_Lesson/js/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const inputRub = document.querySelector('#rub');
+const inputUsd = document.querySelector('#usd');
+
+
+inputRub.addEventListener('input', () => {
+
+   const request = new XMLHttpRequest();
+
+   request.open('GET', 'js/current.json'); // request.open(method, url, async, login, pass);
+   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+   request.send();
+
+   request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+         console.log(request.response);
+         const data = JSON.parse(request.response);
+         inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+      } else {
+         inputUsd.value = "Что-то пошло не так";
+      }
+   });
+
+   //status
+   //statusText
+   //response
+   //readyState
+
+
+
+});
